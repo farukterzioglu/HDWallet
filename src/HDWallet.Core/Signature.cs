28ec4f17be20b24f2f122c946fecc4bcefde8f84
Faruk Terzioğlu <faruk.terzioglu@hotmail.com>
2020-10-12 18:51:25 +0300

    set to netcoreapp3.0

--- src/HDWallet.Core/Signature.cs
+++ src/HDWallet.Core/Signature.cs
@@ -2,8 +2,6 @@ namespace HDWallet.Core
 {
     public class Signature
     {
-        public string SignatureHex { get; set; }
-        public byte[] SignatureBytes => SignatureHex.FromHexToByteArray(); 
         public byte[] R;
         public byte[] S;
         public int RecId;
--- src/HDWallet.Tron/TronSignature.cs
+++ src/HDWallet.Tron/TronSignature.cs
@@ -6,6 +6,7 @@ namespace HDWallet.Tron
     public class TronSignature : Signature
     {
         public byte[] SignatureBytes => Helper.Concat(this.R, this.S, BitConverter.GetBytes(this.RecId));
+        public string SignatureHex => Helper.ToHexString(this.SignatureBytes);
 
         public TronSignature(Signature signature)
         {
