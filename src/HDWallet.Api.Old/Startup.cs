098b8e6fb50ef8b48377a0a714378ab9d7746e70
Faruk Terzioğlu <faruk.terzioglu@hotmail.com>
2020-11-29 22:32:32 +0300

    Remove API

--- src/HDWallet.Api.Old/Startup.cs
+++ src/HDWallet.Api.Old/Startup.cs
@@ -10,7 +10,9 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
 using Microsoft.OpenApi.Models;
+using Swashbuckle.AspNetCore.SwaggerGen;
 
 namespace HDWallet.Api
 {
@@ -23,11 +25,36 @@ namespace HDWallet.Api
 
         public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
+        public class ConfigureSwaggerOptions : IConfigureOptions<SwaggerGenOptions>
+        {
+            readonly IApiVersionDescriptionProvider provider;
+
+            public ConfigureSwaggerOptions( IApiVersionDescriptionProvider provider ) =>
+                this.provider = provider;
+
+            public void Configure( SwaggerGenOptions options )
+            {
+                foreach ( var description in provider.ApiVersionDescriptions )
+                {
+                options.SwaggerDoc(
+                    description.GroupName,
+                    new OpenApiInfo()
+                    {
+                        Title = $"Sample API {description.ApiVersion}",
+                        Version = description.ApiVersion.ToString(),
+                    } );
+                }
+            }
+        }
+
         public void ConfigureServices(IServiceCollection services)
         {
-
+            
             services.AddControllers();
+
+            services.AddApiVersioning();
+            services.AddVersionedApiExplorer( options => options.GroupNameFormat = "'v'VVV" );
+            services.AddTransient<IConfigureOptions<SwaggerGenOptions>, ConfigureSwaggerOptions>();
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "HDWallet.Api", Version = "v1" });
