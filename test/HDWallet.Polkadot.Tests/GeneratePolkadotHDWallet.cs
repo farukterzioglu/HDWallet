73209ebe3203d22e057cae32b6f232586ee35fde
Faruk Terzioğlu <faruk.terzioglu@hotmail.com>
2020-11-30 21:26:31 +0300

    Update polkadot tests

--- test/HDWallet.Polkadot.Tests/GeneratePolkadotHDWallet.cs
+++ test/HDWallet.Polkadot.Tests/GeneratePolkadotHDWallet.cs
@@ -20,7 +20,12 @@ namespace HDWallet.Cardano.Tests
             var privateKey = wallet.PrivateKey;
             var expandedPrivateKey = wallet.ExpandedPrivateKey;
 
-            Console.WriteLine($"Address: {address}");
+            Assert.AreEqual(expected: "5G6tXDeie2KaUgGrwkBfzApKSCm9sE5QvBm1bi1vmBZ4gcCN", actual: address);
+            Assert.AreEqual(expected: "b29b533725c02f6e69d8774c92d8a5a98506c2f09e13a1adbe4db367fbfa512a", actual: publicKey.ToHexString());
+            Assert.AreEqual(expected: "8bd78fe8b30abf91d3e9474c8927d9874fabc7e31ce2d866cf795378161f954a", actual: privateKey.ToHexString());
+            Assert.AreEqual(expected: "8bd78fe8b30abf91d3e9474c8927d9874fabc7e31ce2d866cf795378161f954ab29b533725c02f6e69d8774c92d8a5a98506c2f09e13a1adbe4db367fbfa512a", actual: expandedPrivateKey.ToHexString());
+            
+            Console.WriteLine($"\nAddress: {address}");
             Console.WriteLine($"Public key: {publicKey.ToHexString()}");
             Console.WriteLine($"Private key: {privateKey.ToHexString()}");
             Console.WriteLine($"Expanded private key: {expandedPrivateKey.ToHexString()}");
