645bf30f36757ea8b1faf50355b015859ca4bdd9
Emre Pehlivan <emre.pehlivan@btctrader.com>
2020-12-05 02:29:07 +0300

    Add Tezos wallet

---
+++ src/HDWallet.Tezos/TezosHDWalletSecp256k1.cs
@@ -0,0 +1,24 @@
+﻿using HDWallet.Core;
+using HDWallet.Secp256k1;
+
+namespace HDWallet.Tezos
+{
+    public class TezosHDWalletSecp256k1 : HDWallet<TezosWallet>
+    {
+        private static readonly HDWallet.Core.CoinPath _path = Purpose.Create(PurposeNumber.BIP44).Coin(CoinType.Tezos);
+
+        public TezosHDWalletSecp256k1(string words, string seedPassword = "") : base(words, seedPassword, _path) { }
+
+        /// <summary>
+        /// Generates Account from master. Doesn't derive new path by accountIndexInfo
+        /// </summary>
+        /// <param name="accountMasterKey">Used to generate wallet</param>
+        /// <param name="accountIndexInfo">Used only to store information</param>
+        /// <returns></returns>
+        public static IAccount<TezosWallet> GetAccountFromMasterKey(string accountMasterKey, uint accountIndexInfo)
+        {
+            IAccountHDWallet<TezosWallet> accountHDWallet = new AccountHDWallet<TezosWallet>(accountMasterKey, accountIndexInfo);
+            return accountHDWallet.Account;
+        }
+    }
+}
