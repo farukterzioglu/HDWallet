6d792cd0fb46a1d0c70734d453c1c4b89b72c386
Faruk Terzioğlu <faruk.terzioglu@hotmail.com>
2021-05-03 22:31:14 +0300

    Refactor Tron wallet

--- src/HDWallet.Api/Controllers/V1/TRON/TronHDWalletController.cs
+++ src/HDWallet.Api/Controllers/V1/TRON/TronHDWalletController.cs
@@ -12,20 +12,17 @@ namespace HDWallet.Api.Controllers.V1
     [ApiController]
     [ApiVersion( "1.0" ), ApiVersion("2.0")]
     [Route("api/v{version:apiVersion}/[controller]")]
-    public class TronController : ControllerBase
+    public class TronHDWalletController : ControllerBase
     {
-        private readonly ILogger<TronController> _logger;
+        private readonly ILogger<TronHDWalletController> _logger;
         private readonly IHDWallet<TronWallet> _hDWallet;
-        private readonly IAccountHDWallet<TronWallet> _accountHDWallet;
 
-        public TronController(
-            ILogger<TronController> logger,
-            Func<IHDWallet<TronWallet>> hDWallet, 
-            Func<IAccountHDWallet<TronWallet>> accountHDWallet)
+        public TronHDWalletController(
+            ILogger<TronHDWalletController> logger,
+            Func<IHDWallet<TronWallet>> hDWallet)
         {
             _logger = logger;
             _hDWallet= hDWallet();
-            _accountHDWallet = accountHDWallet();
         }
 
         [HttpGet("/account/{accountNumber}/deposit/{addressIndex}")]
@@ -49,30 +46,6 @@ namespace HDWallet.Api.Controllers.V1
             }
 
             var wallet = _hDWallet.GetAccount(accountNumber).GetInternalWallet(addressIndex);
-            return wallet.Address;
-        }
-
-        [HttpGet("/deposit/{addressIndex}")]
-        public ActionResult<string> GetAccountDeposit(uint addressIndex)
-        {
-            if(_accountHDWallet == null) 
-            {
-                return BadRequest("Wallet wasn't initialized with master key! Use hd wallet.");
-            }
-
-            var wallet = _accountHDWallet.Account.GetExternalWallet(addressIndex);
-            return wallet.Address;
-        }
-
-        [HttpGet("/change/{addressIndex}")]
-        public ActionResult<string> GetAccountChange(uint addressIndex)
-        {
-            if(_accountHDWallet == null) 
-            {
-                return BadRequest("Wallet wasn't initialized with master key! Use hd wallet.");
-            }
-
-            var wallet = _accountHDWallet.Account.GetInternalWallet(addressIndex);
             return wallet.Address;
         }
     }
---
+++ src/HDWallet.Api/Controllers/V1/TRON/TronWalletController.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using HDWallet.Core;
+using HDWallet.Tron;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace HDWallet.Api.Controllers.V1
+{
+    [ApiController]
+    [ApiVersion( "1.0" ), ApiVersion("2.0")]
+    [Route("api/v{version:apiVersion}/[controller]")]
+    public class TronWalletController : ControllerBase
+    {
+        private readonly ILogger<TronWalletController> _logger;
+        private readonly IAccountHDWallet<TronWallet> _accountHDWallet;
+
+        public TronWalletController(
+            ILogger<TronWalletController> logger,
+            Func<IAccountHDWallet<TronWallet>> accountHDWallet)
+        {
+            _logger = logger;
+            _accountHDWallet = accountHDWallet();
+        }
+
+        [HttpGet("/deposit/{addressIndex}")]
+        public ActionResult<string> GetAccountDeposit(uint addressIndex)
+        {
+            if(_accountHDWallet == null) 
+            {
+                return BadRequest("Wallet wasn't initialized with master key! Use hd wallet.");
+            }
+
+            var wallet = _accountHDWallet.Account.GetExternalWallet(addressIndex);
+            return wallet.Address;
+        }
+
+        [HttpGet("/change/{addressIndex}")]
+        public ActionResult<string> GetAccountChange(uint addressIndex)
+        {
+            if(_accountHDWallet == null) 
+            {
+                return BadRequest("Wallet wasn't initialized with master key! Use hd wallet.");
+            }
+
+            var wallet = _accountHDWallet.Account.GetInternalWallet(addressIndex);
+            return wallet.Address;
+        }
+    }
+}
