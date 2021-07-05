a1b0a7b7ef73f53a9989c3c9b87fcb40c08f8ff5
Faruk Terzioğlu <faruk.terzioglu@hotmail.com>
2021-07-05 23:56:25 +0300

    Fix solana unit tests

--- test/HDWallet.Solana.Tests/UnitTest1.cs
+++ test/HDWallet.Solana.Tests/UnitTest1.cs
@@ -47,19 +47,21 @@ namespace HDWallet.Solana.Tests
         public void ShouldGenerateFromPrivateKey()
         {
             var privateKey = "f5e5767cf153319517630f226876b86c8160cc583bc013744c6bf255f5cc0ee5";
-            // fkdKBkdNytEWk6fChKNy6j8Ni546y5PzVDNHAUwuyp8TSVzZJcwVt1XXfifMiQZzf6yfxhjKNw9bWRezgpkSvFw
-            // f5e5767cf153319517630f226876b86c8160cc583bc013744c6bf255f5cc0ee5
-            
             SolanaWallet wallet = new SolanaWallet(privateKey);
-            Assert.AreEqual(expected: "87VZwKPMxBU8zppWB77ySodMfBf2HKWT3N6nKmzUiVBb", actual: wallet.Address);
+            Assert.AreEqual(expected: "3fD58whN2KJaN9T4r5uE3ELFmzRW1dQNuszrmC6gnhx1", actual: wallet.Address);
+
+            Console.WriteLine(wallet.Address);
+            Console.WriteLine($"[{string.Join(", ", wallet.ExpandedPrivateKey)}]");
         }
 
-        [Test]
-        public void ShouldGenerateFromBytes()
+        [TestCase("73pneJadR1WwkgJGH7btmHyoMVE5cSrU2QfMuiikD7vs", new byte[] { 64,195,121,69,216,110,229,136,27,133,1,140,181,138,22,178,198,20,15,35,48,171,4,30,180,37,169,22,13,160,106,91})]
+        [TestCase("2q7pyhPwAwZ3QMfZrnAbDhnh9mDUqycszcpf86VgQxhF", new byte[] { 153,218,149,89,225,94,145,62,233,171,46,83,227,223,173,87,93,163,59,73,190,17,37,187,146,46,51,73,79,73,136,40})]
+        [TestCase("CK3UH5tLou7koGtaZtHGkGQyL9bNi3ejobJFuzL543sQ", new byte[] { 198,56,227,12,161,54,119,141,203,235,180,241,24,4,183,102,251,49,224,206,89,157,65,151,183,254,58,245,79,240,194,30})]
+        [TestCase("U9N2BXbxhc93pMdhWj8XaTvJ3rfKoGCNWULgPBPB8kS", new byte[] { 31,204,135,36,57,87,92,52,152,229,242,233,174,6,40,79,180,237,58,253,127,184,20,146,83,208,72,137,172,228,238,176})]
+        public void ShouldGenerateFromBytes(string address, byte[] privateKeyBytes)
         {
-            var privateKeyBytes = new byte[] { 64,195,121,69,216,110,229,136,27,133,1,140,181,138,22,178,198,20,15,35,48,171,4,30,180,37,169,22,13,160,106,91,89,223,80,232,234,49,158,82,7,219,9,174,107,237,201,108,18,171,161,194,197,206,93,48,234,129,72,82,78,117,35,124 };
             SolanaWallet wallet = new SolanaWallet(privateKeyBytes);
-            Assert.AreEqual(expected: "73pneJadR1WwkgJGH7btmHyoMVE5cSrU2QfMuiikD7vs", actual: wallet.Address);
+            Assert.AreEqual(expected: address, actual: wallet.Address);
         }
 
         [Test]
